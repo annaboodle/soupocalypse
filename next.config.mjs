51355964a940523ff6e5fba8bf0f9438c2cfceed
Annabeth Carroll <annabethcarroll@gmail.com>
2024-02-16 15:47:12 -0800

    rebuild to fix error in export

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 // const nextConfig = {};
 
 const nextConfig = {
+  output: "export",
   basePath: "/soupocalypse",
   assetPrefix: "/soupocalypse/",
   trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
