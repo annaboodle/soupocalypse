d936c5a16429060061fbb1aad21fa7356bece1cc
Annabeth Carroll <annabethcarroll@gmail.com>
2026-01-21 10:07:50 -0800

    Fix basePath/assetPrefix for custom domain

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"BEv6jcRXMG9BmnankxmoNZ07F8hCbkKmhivkl0VXMWg=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,13 @@
 const nextConfig = {
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-
-  // when DEVELOPING and running locally, comment out:
   output: "export",
-  basePath: "/soupocalypse",
-  assetPrefix: "/soupocalypse/",
   images: { unoptimized: true },
+
+  // this should do it while the site is hosted on custom domain (soupocalypse.com), however
+  // if running on annaboodle.github.io/soupocalypse/ and you see a 404 on localhost, uncomment these:
+  // basePath: "/soupocalypse",
+  // assetPrefix: "/soupocalypse/",
 };
 
 export default nextConfig;
