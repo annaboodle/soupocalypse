5d2cdecb2661c84280588973c15aa0122f525a9d
Annabeth Carroll <annabethcarroll@gmail.com>
2026-01-22 11:15:49 -0800

    Fix font warning by loading fonts via next/font/google

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,15 +2,24 @@ import Script from 'next/script';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./globals.css";
+import { League_Gothic, Merriweather_Sans } from "next/font/google";
 
 export const metadata = {
   title: "Soupocalypse",
   description: "It's not the end of the world until we run out of soup!",
 };
 
+const leagueGothic = League_Gothic({ subsets: ["latin"], weight: ["400"], variable: "--font-league" });
+const merriweatherSans = Merriweather_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "600", "800"],
+  variable: "--font-merriweather-sans",
+});
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${leagueGothic.variable} ${merriweatherSans.variable}`}>
 
       {/* adding google analytics tracking */}
       <Script
@@ -33,10 +42,6 @@ export default function RootLayout({ children }) {
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Merriweather+Sans:ital,wght@0,300..800;1,300..800&display=swap"
-          rel="stylesheet"
         />
       </head>
 
